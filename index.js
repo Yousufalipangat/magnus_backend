7d1c1a8de9dba724505209f7634195faedf81092
Yousufalipangat <105538201+Yousufalipangat@users.noreply.github.com>
2023-08-16 16:10:41 +0530

    cors update

--- index.js
+++ index.js
@@ -7,18 +7,33 @@ const app = express();
 
 
 app.use(bodyParser.json())
-const corsOptions ={
-    origin:'https://magnus-full-stack-v1.netlify.app', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
 
-app.use(cors(corsOptions));
+var whitelist = ['https://magnus-full-stack-v1.netlify.app', 'http://localhost:3000']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+ 
+
+// const corsOptions ={
+//     origin:['https://magnus-full-stack-v1.netlify.app','http://localhost:3000'], 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
+
+app.use(cors(corsOptionsDelegate));
 app.use("/record",records);
 
 app.post('/' ,bodyParser.urlencoded({extended:false}), (req,res)=>{
    
+   
     if('train@urself.com' == req.body.email && 'jobprogram' == req.body.pass){
+        
         res.send(true);
     }else{
         res.send(false)
