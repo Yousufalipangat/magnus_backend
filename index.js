120838ba690873bd1438ca49451d379f7381b33a
Yousufalipangat <105538201+Yousufalipangat@users.noreply.github.com>
2023-08-18 10:03:32 +0530

    magnus-backend-point.onrender.com

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(session(
     resave:true,
     saveUninitialized:false,
     cookie:{
-      domain:'magnus-backend-point.onrender',
+      domain:'magnus-backend-point.onrender.com',
       secure:true,
       maxAge:1000*60*5,
       sameSite:'none',
