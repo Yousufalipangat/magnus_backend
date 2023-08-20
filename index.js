595f858a79bbb5f98f236c91bb9c8dde2a3c20c1
Yousufalipangat <105538201+Yousufalipangat@users.noreply.github.com>
2023-08-20 16:26:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import session from 'express-session';
 //import cookieParser from 'cookie-parser';
 app.use(express.json())
 //app.use(cookieParser());
-app.use(cors({origin:'https://magnus-full-stack-v1.netlify.app',credentials:true}));
+app.use(cors({origin:'https://magnus-full-stack-v1.netlify.app/*',credentials:true}));
 app.options(cors({origin:'https://magnus-full-stack-v1.netlify.app',credentials:true}))
 app.use('/record',records);
 
